c5f14f9101c4ebffe90d6a5917f469eb0b84b8f1
Ifty <shadnan31@gmail.com>
2026-08-13 05:20:33 +0600

    day-11: added problem 5 to the soutions file

--- day-11/solutions.js
+++ day-11/solutions.js
@@ -44,3 +44,39 @@ const allProductsInStock = products => {
     }
     return products.length > 0 && products.every(product => product.stock > 0) 
 }
+
+// 🔴 Problem 5 ⭐ Daily Challenge — Course Analyzer
+const analyzeCourses = courses => {
+
+    // validation starts
+    if (!Array.isArray(courses)){
+        return 'Invalid'
+    }
+    if (courses.some(course => Array.isArray(course) || typeof course !== 'object' || course === null || !Number.isFinite(course.id) || typeof course.title !== 'string' || course.title.trim().length === 0 || !Number.isFinite(course.enrolled) || typeof course.active !== 'boolean')){
+        return 'Invalid'
+    }
+    // validation ends
+
+    const activeCourses = courses.filter(course => course.active).map(course => course.title);
+    const totalEnrollment = courses.reduce((accumulator,currentValue)=> accumulator + currentValue.enrolled, 0);
+    const hasInactiveCourse = courses.some(course => !course.active);
+    const allCoursesHaveStudents = courses.every(course => course.enrolled > 0);
+
+    let mostPopularCourse;
+    let mostEnrolled = 0
+    courses.forEach(course => {
+        if (course.enrolled > mostEnrolled){
+            mostEnrolled = course.enrolled
+            mostPopularCourse = course.title
+        }
+    })
+   
+    return {
+        activeCourses,
+        totalEnrollment,
+        hasInactiveCourse,
+        allCoursesHaveStudents,
+        mostPopularCourse
+    }
+}
+
